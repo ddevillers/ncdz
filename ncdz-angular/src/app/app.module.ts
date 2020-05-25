c7d932be5c2bd2d190c1c952963e35724b960409
Antrootz <toto-smx@hotmail.fr>
2020-05-25 14:25:13 +0200

    Init angular

--- ncdz-angular/src/app/app.module.ts
+++ ncdz-angular/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PortailComponent } from './portail/portail.component';
+import { MembreComponent } from './membre/membre.component';
+import { MaterielComponent } from './materiel/materiel.component';
+import { VolComponent } from './vol/vol.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortailComponent,
+    MembreComponent,
+    MaterielComponent,
+    VolComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule
