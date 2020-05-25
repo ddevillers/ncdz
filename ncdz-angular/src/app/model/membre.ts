5f7c303f92fceb7dec95a7259ffd86a0275c2c7f
Delphine <del.devillers@gmail.com>
2020-05-25 14:55:18 +0200

    membre+pilote

--- ncdz-angular/src/app/model/membre.ts
+++ ncdz-angular/src/app/model/membre.ts
@@ -1,2 +1,11 @@
 export class Membre {
+
+    constructor(
+        public id: number, 
+        public nom?: string, 
+        public prenom?: string, 
+        public numeroLicence?: number, 
+        public dateLicence?: Date, 
+        public niveau?: string
+        ) { }
 }
--- ncdz-angular/src/app/model/pilote.ts
+++ ncdz-angular/src/app/model/pilote.ts
@@ -1,2 +1,18 @@
-export class Pilote {
+import { Membre } from './membre';
+import { Avion } from './avion';
+
+export class Pilote extends Membre {
+
+    public avions: Array<Avion>
+
+    constructor(
+        public id: number, 
+        public nom?: string, 
+        public prenom?: string, 
+        public numeroLicence?: number,
+        avions?: Array<Avion>) 
+    {
+        super(id, nom, prenom, numeroLicence);
+        this.avions = avions;
+     }
 }
