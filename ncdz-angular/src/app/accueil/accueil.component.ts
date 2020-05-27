aeb4cf6f30f3f32aa3cbde51782a726eeb9e646b
Florian Massol <florian.massol.pro@gmail.com>
2020-05-27 15:40:31 +0200

    erreur ts

--- ncdz-angular/src/app/accueil/accueil.component.ts
+++ ncdz-angular/src/app/accueil/accueil.component.ts
@@ -27,16 +27,5 @@ export class AccueilComponent implements OnInit {
     this.router.navigate(['/portail'])
   }
 
-  public function() {
-
-    let mouseX, mouseY;
-     
-
-
-    @HostListener('document:mousemove', ['$event']) 
-  onMouseMove(e) {
-    console.log(e);
-  }
-  }
 
 }
